50a6bb747971e3dc4c143d7a83f004eccf994f46
nkalupahana <avishkank@gmail.com>
2023-12-08 14:02:38 -0600

    Add basic math tests

---
+++ app/src/main/java/conv/Converter.java
@@ -0,0 +1,25 @@
+package conv;
+
+import java.util.List;
+
+public class Converter {
+    public int add(int a, int b) {
+        // Add numbers together
+        return 0;
+    }
+
+    public int addPositive(int a, int b, int c) {
+        // Add only positive numbers together (ignore negative numbers)
+        return 0;
+    }
+
+    public int addList(List<Integer> list) {
+        // Add numbers together
+        return 0;
+    }
+
+    public int addPositiveList(List<Integer> list) {
+        // Add only positive numbers together (ignore negative numbers)
+        return 0;
+    }
+}
